fd21b31f04ee98435347bf7b0fdd3de65f0d965d
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-06-21 18:04:32 +0200

    add: h1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,11 @@
 // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 // Bonus: Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+// ex
+
+var app = new Vue({ 
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    }
+});

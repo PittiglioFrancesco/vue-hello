e8c5026e050adac8926d489ee7af0d3cdf9ee2ae
PittiglioFrancesco <francescopittiglio79@gmail.com>
2021-06-21 18:07:42 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@
 var app = new Vue({ 
     el: '#app',
     data: {
-        message: 'Hello Vue!'
+        message: 'Hello Vue!',
+        img: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fimgflip.com%2Fmemetemplate%2F316634937%2FThink-Mark-Think&psig=AOvVaw3H8J-P2DJW1K2jbOd3Rjj9&ust=1624377972345000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCPCzqdeNqfECFQAAAAAdAAAAABAD"
     }
 });
